1eeceb5c099464b57f0649d49aa21cae57a18da8
Alex Bonder <lexbonder@gmail.com>
2024-03-17 15:14:52 -0400

    Comment out unused code

--- src/store/skill-context.tsx
+++ src/store/skill-context.tsx
@@ -2,7 +2,7 @@ import { ReactNode, createContext, useReducer } from 'react';
 import { v4 as uuid } from 'uuid';
 import { Actions, ISkill, ISkillContext } from '../types';
 import { ADD_SKILL, DELETE_SKILL, EDIT_SKILL, isGenericAction } from '../action-types';
-import { testData } from '../mockData/test-skills';
+// import { testData } from '../mockData/test-skills';
 
 export const SkillContext = createContext<ISkillContext>({
     skills: [],
